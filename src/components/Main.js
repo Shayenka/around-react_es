20dd1fb355efdd0471814af5b83b54f432e4b503
Shayenka Alvarado <shayenkalvarado@gmail.com>
2023-05-24 15:37:59 -0600

    Cargar cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../utils/api";
 import editprofile from "../images/Edit.svg";
 import addcard from "../images/Signo+.svg";
 import deleteCard from "../images/Papelera.svg";
 import likeCard from "../images/like.svg";
 
 function Main({ onEditProfileClick, onAddPlaceClick, onEditAvatarClick }) {
+  const [userName, setUserName] = useState("");
+  const [userAbout, setUserDescription] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    api
+      .getUserInfo()
+      .then((response) => {
+        setUserName(response.name);
+        setUserDescription(response.about);
+        setUserAvatar(response.avatar);
+      })
+      .catch((error) => {
+        console.log("Error al obtener los datos del usuario:", error);
+      });
+
+    api
+      .getCards()
+      .then((response) => {
+        setCards([response]);
+      })
+      .catch((error) => {
+        console.log("Error al obtener los datos de las tarjetas:", error);
+      });
+  }, []);
+
   return (
     <main>
       <section className="profile">
         <div className="profile__avatar" onClick={onEditAvatarClick}>
-          <img className="profile__image" src=" " alt="Foto de Perfil" />
+          <img
+            className="profile__image"
+            src={userAvatar}
+            alt="Foto de Perfil"
+            style={{ backgroundImage: `url(${userAvatar})` }}
+          />
         </div>
 
         <div className="profile__info">
           <div>
-            <h1 className="profile__name"></h1>
-            <h2 className="profile__occupation"></h2>
+            <h1 className="profile__name">{userName}</h1>
+            <h2 className="profile__occupation">{userAbout}</h2>
           </div>
           <button className="profile__edit" onClick={onEditProfileClick}>
             <img
@@ -36,27 +69,33 @@ function Main({ onEditProfileClick, onAddPlaceClick, onEditAvatarClick }) {
 
       <section className="elements">
         <div>
-          <template id="templateCard">
-            <div className="element">
+          {cards.map((card) => (
+            <div className="element" key={card._id}>
               <img
                 className="element__delete"
                 src={deleteCard}
                 alt="Icono de papelera para eliminar"
               />
-              <img className="element__image" id="src" src=" " alt=" " />
+              <img
+                className="element__image"
+                id="src"
+                src={card.link}
+                alt={card.name}
+                style={{ backgroundImage: `url(${card.link})` }}
+              />
               <div className="element__footer-photo">
-                <h3 className="element__title"></h3>
+                <h3 className="element__title">{card.name}</h3>
                 <div className="likes-card">
                   <img
                     className="icon-like"
                     src={likeCard}
                     alt="Icono de corazón"
                   />
-                  <span className="likes-card__count">1</span>
+                  <span className="likes-card__count">{card.likes}</span>
                 </div>
               </div>
             </div>
-          </template>
+          ))}
         </div>
       </section>
 
---
+++ src/utils/api.js
@@ -0,0 +1,104 @@
+class Api {
+  constructor() {
+    this.authorization = "3270d03d-8b4c-49a2-869b-f096d27af6a5";
+  }
+
+  _useFetch(url, method, body) {
+    return fetch(url, {
+      headers: {
+        authorization: this.authorization,
+        "Content-Type": "application/json",
+      },
+      method,
+      body: JSON.stringify(body),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  getUserInfo() {
+    return this._useFetch(
+      "https://around.nomoreparties.co/v1/web_es_05/users/me",
+      `GET`
+    ).then((result) => {
+      return result;
+    });
+  }
+
+  getCards() {
+    return this._useFetch(
+      "https://around.nomoreparties.co/v1/web_es_05/cards",
+      `GET`
+    ).then((result) => {
+      return result;
+    });
+  }
+
+  editUserInfo(userData) {
+    return this._useFetch(
+      "https://around.nomoreparties.co/v1/web_es_05/users/me",
+      `PATCH`,
+      userData
+    ).then((result) => {
+      return result;
+    });
+  }
+
+  addNewCard(cardData) {
+    return this._useFetch(
+      "https://around.nomoreparties.co/v1/web_es_05/cards",
+      `POST`,
+      cardData
+    ).then((result) => {
+      return result;
+    });
+  }
+
+  addLike(cardId) {
+    return this._useFetch(
+      `https://around.nomoreparties.co/v1/web_es_05/cards/likes/${cardId}`,
+      `PUT`
+    ).then((result) => {
+      return result.likes;
+    });
+  }
+
+  removeLike(cardId) {
+    return this._useFetch(
+      `https://around.nomoreparties.co/v1/web_es_05/cards/likes/${cardId}`,
+      `DELETE`
+    ).then((result) => {
+      return result.likes;
+    });
+  }
+
+  deleteCard(cardId) {
+    return this._useFetch(
+      `https://around.nomoreparties.co/v1/web_es_05/cards/${cardId}`,
+      `DELETE`
+    ).then((result) => {
+      return result;
+    });
+  }
+
+  changeAvatarProfile(userAvatar) {
+    return this._useFetch(
+      "https://around.nomoreparties.co/v1/web_es_05/users/me/avatar",
+      `PATCH`,
+      userAvatar
+    ).then((result) => {
+      return result;
+    });
+  }
+}
+
+const api = new Api({
+  address: "https://nomoreparties.co",
+  groupId: `web_es_05`, // CHANGE IT WITH YOUR COHORT
+  token: `3270d03d-8b4c-49a2-869b-f096d27af6a5`, // CHANGE IT WITH YOUR TOKEN
+});
+
+export default api;
